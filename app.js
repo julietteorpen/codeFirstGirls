c574463e85c2769dab4a2838be95b7cac0f3c1a2
neprojo <53294140+neprojo@users.noreply.github.com>
2019-12-03 18:05:34 +0000

    Created stories pages and alerts

---
+++ app.js
@@ -0,0 +1,10 @@
+alert(`Would you like to share your experience of being a woman working in the tech industry?"`);
+var answer = prompt("Would you like to share your experience of being a woman working in the tech industry?");
+
+if (answer == 'Yes') {
+    alert('Great! Please fill in the form below to share your story!');
+} else if (answer == 'Okay') {
+    alert( 'Great! I can tell you like breaking the rules!');
+} else {
+    alert('Maybe another time, have a read of other stories to inspire you');
+}
